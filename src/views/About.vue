9b1d20b1292559cccb7a57d56537867b85bcf074
CavinHuang <sujinw@qq.com>
2020-10-26 10:36:20 +0800

    back

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="toggleModal">打开Modal对话框</button>
+    <Modal v-show="showModal" v-on:close="closeme"></Modal>
   </div>
 </template>
+
+<script>
+import Modal from '@/components/Modal'
+export default {
+  name: 'About',
+  data () {
+    return {
+      showModal: false
+    }
+  },
+  components: {
+    Modal
+  },
+  methods: {
+    toggleModal () {
+      this.showModal = !this.showModal
+    },
+    closeme () {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
