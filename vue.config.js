bbc408f401f30ed246df1c471298dcf8aab6d0a2
CavinHuang <sujinw@qq.com>
2020-11-12 11:31:22 +0800

    修改路径

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/**
+ * vue 配置文件
+ * @author huangchunmao
+ * @email sujinw@qq.com
+ * @version v1.0.0
+ * @date 2020/11/12
+*/
+
+module.exports = {
+  publicPath: '/docs', // 部署应用包时的根路径
+  runtimeCompiler: false, // 是否使用包含运行时编译器的 Vue 构建版本。
+  productionSourceMap: false, // 关闭生产环境source map，加速生产环境构建，并减少包体积
+  lintOnSave: true, // 保存时 lint 代码
+}
