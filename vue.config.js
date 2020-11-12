72279811e1c32021ee8d7b88106f5c985d4d7b72
CavinHuang <sujinw@qq.com>
2020-11-12 11:43:50 +0800

    update gh page

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@
 */
 
 module.exports = {
-  publicPath: './docs', // 部署应用包时的根路径
+  publicPath: '/vue-quill-editor/docs', // 部署应用包时的根路径
   runtimeCompiler: false, // 是否使用包含运行时编译器的 Vue 构建版本。
   productionSourceMap: false, // 关闭生产环境source map，加速生产环境构建，并减少包体积
   lintOnSave: true, // 保存时 lint 代码
